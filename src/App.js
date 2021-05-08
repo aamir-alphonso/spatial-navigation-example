8e15cb077ef0978dc2f64507f42e44b5ec065994
mdaamirkhan <aamir@alphonso.tv>
2021-05-08 12:25:56 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {PureComponent} from "react"
+import {initNavigation, setKeyMap, withFocusable} from '@noriginmedia/react-spatial-navigation';
+
+initNavigation();
+
+// Optional
+setKeyMap({
+  'left': 37,
+  'up': 38,
+  'right': 39,
+  'down': 40,
+  'enter': 13
+});
+
+const Component2 = (props) => {
+  console.log(props)
+  const divStyle={
+    color: 'blue',
+  }
+    return(
+      <div style={props.focused ? divStyle : null}>
+        Hello World!!
+      </div>
+    )
+}
+
+const FocusableComponent2 = withFocusable()(Component2)
+
+
+const Component = ({focused, setFocus}) => (<div>
+  Hello World!
+</div>);
+
+const FocusableComponent = withFocusable()(Component);
+
+const Menu = (props) => {
+  props.setFocus();
+  const onEnterPress = () => {
+    console.log("Enter pressed and working")
+  }
+  return(<>
+  <FocusableComponent2 />
+  <FocusableComponent2 onEnterPress={onEnterPress}/>
+  <FocusableComponent2 />
+  <FocusableComponent2 />
+  </>)
+}
+
+const MenuFocusable = withFocusable()(Menu)
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <MenuFocusable />
     </div>
   );
 }
